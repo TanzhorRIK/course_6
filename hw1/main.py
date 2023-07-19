62b723a8f84849314194892c12a52b1a8f8bc69c
Tanzhor_RIK <tanzhor4@gmail.com>
2023-07-19 21:23:29 +0700

    Initial commit

---
+++ hw1/main.py
@@ -0,0 +1,37 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_page(self):
+        with open("contacts.html", encoding="utf-8") as in_f:
+            content = in_f.read()
+        return content
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        page_address = query_components.get('page')
+        page_content = self.__get_page()
+        if page_address:
+            page_content = self.__get_blog_article(page_address[0])
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
